2f74c286cca779d26323a020d128f62053c17442
Halikk <h_hasar@hotmail.com>
2024-11-09 00:24:16 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+string s = "A man, a plan, a canal: Panama";
+
+s = s.ToLower();
+s = String.Concat(s.Where(char.IsLetterOrDigit));
+char[] chars= s.ToCharArray();
+Array.Reverse(chars);
+string s_temp = new string(chars);
+
+Console.WriteLine(s== s_temp);
